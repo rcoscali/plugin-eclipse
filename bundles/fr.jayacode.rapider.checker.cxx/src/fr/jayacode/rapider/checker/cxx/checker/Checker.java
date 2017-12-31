6fa13b87fe092920db0aa6e6c3b33b5368b0bc20
cconversin <cconversin@gmail.com>
2017-12-31 11:18:22 +0100

    Gestion des commentaires de suppression des problèmes : si la ligne du problème comporte le commentaire de suppression du problème, on le filtre

--- bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/checker/Checker.java
+++ bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/checker/Checker.java
@@ -3,7 +3,11 @@
  */
 package fr.jayacode.rapider.checker.cxx.checker;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -12,10 +16,14 @@ import java.util.Map;
 import org.eclipse.cdt.codan.core.cxx.externaltool.AbstractExternalToolBasedChecker;
 import org.eclipse.cdt.codan.core.cxx.externaltool.ConfigurationSettings;
 import org.eclipse.cdt.codan.core.model.IChecker;
+import org.eclipse.cdt.codan.core.model.IProblem;
+import org.eclipse.cdt.codan.core.model.IProblemLocation;
 import org.eclipse.cdt.codan.core.model.IProblemWorkingCopy;
 import org.eclipse.cdt.core.IMarkerGenerator;
 import org.eclipse.cdt.core.ProblemMarkerInfo;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 
 import fr.jayacode.rapider.checker.cxx.Messages;
 
@@ -27,14 +35,13 @@ import fr.jayacode.rapider.checker.cxx.Messages;
 public class Checker extends AbstractExternalToolBasedChecker implements IChecker {
 
 	private static final String RAPIDER_TOOL_NAME = Messages.Checker_RapiderToolName;
-	static private Collection<String> PROCESSED_EXTENSIONS = Arrays.asList("cpp", "CPP", "c", "C");  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$//$NON-NLS-4$
+	static private Collection<String> PROCESSED_EXTENSIONS = Arrays.asList("cpp", "CPP", "c", "C"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$//$NON-NLS-4$
 
 	public Checker() {
 		super(new ConfigurationSettings(RAPIDER_TOOL_NAME,
 				new File("/home/cconversin/workspace/FakeRapider/Release/FakeRapider"), "")); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
-	
 	@Override
 	public boolean processResource(IResource resource) {
 		if (PROCESSED_EXTENSIONS.contains(resource.getFileExtension())) {
@@ -84,6 +91,37 @@ public class Checker extends AbstractExternalToolBasedChecker implements IChecke
 	@Override
 	protected String getReferenceProblemId() {
 		return ERROR_PROBLEM_ID;
+	}
+
+	/* (non-Javadoc)
+	 * Filtre pour les commentaires de suppression du problème : si la ligne contient le commentaire, alors on ne remonte pas le problème
+	 * comme étant remonté à l'utilisateur
+	 * @see org.eclipse.cdt.codan.core.model.AbstractCheckerWithProblemPreferences#shouldProduceProblem(org.eclipse.cdt.codan.core.model.IProblem, org.eclipse.cdt.codan.core.model.IProblemLocation, java.lang.Object[])
+	 */
+	@Override
+	protected boolean shouldProduceProblem(IProblem problem, IProblemLocation loc, Object... args) {
+		String suppressionComment = (String) getSuppressionCommentPreference(problem).getValue();
+		if (suppressionComment.isEmpty()) {
+			return super.shouldProduceProblem(problem, loc, args);
+		}
+		IResource resource = loc.getFile();
+		if (!(resource instanceof IFile)) {
+			return false;
+		}
+		IFile file = (IFile) resource;
+		try {
+			InputStream content = file.getContents();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+			String line = new String();
+			int n = loc.getLineNumber();
+			for (int i = 0; i < loc.getLineNumber(); i++) {
+				line = reader.readLine();
+			}
+			return !line.contains(suppressionComment);
+		} catch (CoreException | IOException e) {
+			// TODO : print stack ?
+		}
+		return super.shouldProduceProblem(problem, loc, args);
 	}
 
 }
