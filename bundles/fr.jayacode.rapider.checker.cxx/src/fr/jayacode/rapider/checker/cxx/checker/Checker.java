1f6607cc572319ea6cad2f95bcc5a1b9608810b7
cconversin <cconversin@gmail.com>
2018-01-22 16:58:32 +0100

    Evolution du parseur et du checker. Pas encore fonctionnel.

--- bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/checker/Checker.java
+++ bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/checker/Checker.java
@@ -8,20 +8,41 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.eclipse.cdt.codan.core.cxx.externaltool.AbstractExternalToolBasedChecker;
+import org.eclipse.cdt.codan.core.CodanRuntime;
+import org.eclipse.cdt.codan.core.cxx.Activator;
+import org.eclipse.cdt.codan.core.cxx.externaltool.ArgsSeparator;
 import org.eclipse.cdt.codan.core.cxx.externaltool.ConfigurationSettings;
-import org.eclipse.cdt.codan.core.model.IChecker;
+import org.eclipse.cdt.codan.core.cxx.externaltool.IInvocationParametersProvider;
+import org.eclipse.cdt.codan.core.cxx.externaltool.InvocationFailure;
+import org.eclipse.cdt.codan.core.cxx.externaltool.InvocationParameters;
+import org.eclipse.cdt.codan.core.cxx.externaltool.InvocationParametersProvider;
+import org.eclipse.cdt.codan.core.cxx.externaltool.SingleConfigurationSetting;
+import org.eclipse.cdt.codan.core.cxx.internal.externaltool.ExternalToolInvoker;
+import org.eclipse.cdt.codan.core.model.AbstractCheckerWithProblemPreferences;
+import org.eclipse.cdt.codan.core.model.CheckerLaunchMode;
 import org.eclipse.cdt.codan.core.model.IProblem;
 import org.eclipse.cdt.codan.core.model.IProblemLocation;
+import org.eclipse.cdt.codan.core.model.IProblemLocationFactory;
 import org.eclipse.cdt.codan.core.model.IProblemWorkingCopy;
+import org.eclipse.cdt.codan.core.param.IProblemPreference;
+import org.eclipse.cdt.codan.core.param.MapProblemPreference;
+import org.eclipse.cdt.codan.core.param.RootProblemPreference;
+import org.eclipse.cdt.codan.core.param.SharedRootProblemPreference;
+import org.eclipse.cdt.core.ErrorParserManager;
+import org.eclipse.cdt.core.IConsoleParser;
 import org.eclipse.cdt.core.IMarkerGenerator;
 import org.eclipse.cdt.core.ProblemMarkerInfo;
+import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 
@@ -29,36 +50,188 @@ import fr.jayacode.rapider.checker.cxx.Messages;
 
 /**
  * @author cconversin
+ * TODO : demander à Rémy de me donner les descriptions des règles pour que je puisse les mettre dans les fichiers de messages
  *
  */
 @SuppressWarnings("boxing")
-public class Checker extends AbstractExternalToolBasedChecker implements IChecker {
+public class Checker extends AbstractCheckerWithProblemPreferences implements IMarkerGenerator {
 
 	private static final String RAPIDER_TOOL_NAME = Messages.Checker_RapiderToolName;
-	static private Collection<String> PROCESSED_EXTENSIONS = Arrays.asList("cpp", "CPP", "c", "C"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$//$NON-NLS-4$
+	private static final Collection<String> PROCESSED_EXTENSIONS = Arrays.asList("cpp", "CPP", "c", "C"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$//$NON-NLS-4$
+
+	/**
+	 * These problem ids will be used for errors whom rules can not map any of the
+	 * plugin registered problems. Cf. {@link mapProblemId}.
+	 */
+	private static final String GENERIC_ERROR_ID = "fr.jayacode.rapider.checker.cxx.otherError"; //$NON-NLS-1$
+	private static final String GENERIC_WARNING_ID = "fr.jayacode.rapider.checker.cxx.otherWarning"; //$NON-NLS-1$
+	private static final String GENERIC_INFO_ID = "fr.jayacode.rapider.checker.cxx.otherInfo"; //$NON-NLS-1$
+
+	/**
+	 * This prefix will be concatenated with replacement text to store quickfix
+	 * patches in Codan markers. Cf.
+	 * {@link fr.jayacode.rapider.checker.cxx.quickfix.MarkerResolution}.
+	 */
+	private static final String REPLACEMENT_TEXT_PREFIX = "rapiderreplacementtext:"; //$NON-NLS-1$
+
+	private final ExternalToolInvoker externalToolInvoker;
+	private final IInvocationParametersProvider parametersProvider;
+	private final ArgsSeparator argsSeparator;
+	private final ConfigurationSettings settings;
+	private final RootProblemPreference preferences;
+
+	private List<String> problemIds = new ArrayList<String>();
 
 	public Checker() {
-		super(new ConfigurationSettings(RAPIDER_TOOL_NAME,
-				new File("/home/cconversin/workspace/FakeRapider/Release/FakeRapider"), "")); //$NON-NLS-1$ //$NON-NLS-2$
+		this(new InvocationParametersProvider(), new ArgsSeparator(), new ConfigurationSettings(RAPIDER_TOOL_NAME,
+				new File("/home/cconversin/workspace/FakeRapider/Release/FakeRapider"), ""));
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param parametersProvider
+	 *            provides the parameters to pass when invoking the external tool.
+	 * @param argsSeparator
+	 *            separates the arguments to pass to the external tool executable.
+	 *            These arguments are stored in a single {@code String}.
+	 * @param settings
+	 *            user-configurable external tool configuration settings.
+	 */
+	public Checker(IInvocationParametersProvider parametersProvider, ArgsSeparator argsSeparator,
+			ConfigurationSettings settings) {
+		this.parametersProvider = parametersProvider;
+		this.argsSeparator = argsSeparator;
+		this.settings = settings;
+		this.externalToolInvoker = new ExternalToolInvoker();
+		this.preferences = new SharedRootProblemPreference();
+	}
+
+	/**
+	 * Returns {@code false} because this checker cannot run "as you type" by
+	 * default.
+	 * 
+	 * @return {@code false}.
+	 */
+	@Override
+	public boolean runInEditor() {
+		return false;
 	}
 
 	@Override
 	public boolean processResource(IResource resource) {
 		if (PROCESSED_EXTENSIONS.contains(resource.getFileExtension())) {
-			return super.processResource(resource);
+			this.process(resource);
 		}
 		return false;
 	}
 
-	@Override
-	public void initPreferences(IProblemWorkingCopy problem) {
-		// TODO : ne faire ça qu'une seule fois
-		super.initPreferences(problem);
+	private void process(IResource resource) {
+		try {
+			InvocationParameters parameters = this.parametersProvider.createParameters(resource);
+			if (parameters != null) {
+				invokeRapider(parameters);
+			}
+		} catch (Throwable error) {
+			logResourceProcessingFailure(error, resource);
+		}
+	}
+
+	
+	/**
+	 * Invoke Rapider
+	 * @param parameters
+	 * @throws Throwable
+	 */
+	private void invokeRapider(InvocationParameters parameters) throws Throwable {
+		IConsoleParser[] parsers = new IConsoleParser[] { createErrorParserManager(parameters) };
+		try {
+			this.externalToolInvoker.invoke(parameters, this.settings, this.argsSeparator, parsers);
+		} catch (InvocationFailure error) {
+			handleInvocationFailure(error, parameters);
+		}
+	}
+
+	private ErrorParserManager createErrorParserManager(InvocationParameters parameters) {
+		IProject project = parameters.getActualFile().getProject();
+		URI workingDirectory = URIUtil.toURI(parameters.getWorkingDirectory());
+		return new ErrorParserManager(project, workingDirectory, this, getParserIDs());
+	}
+
+	/**
+	 * Handles a failure reported when invoking the external tool. This
+	 * implementation simply logs the failure.
+	 * 
+	 * @param error
+	 *            the reported failure.
+	 * @param parameters
+	 *            the parameters passed to the external tool executable.
+	 */
+	protected void handleInvocationFailure(InvocationFailure error, InvocationParameters parameters) {
+		logResourceProcessingFailure(error, parameters.getActualFile());
+	}
+
+	private static void logResourceProcessingFailure(Throwable error, IResource resource) {
+		String location = resource.getLocation().toOSString();
+		String msg = String.format("Unable to process resource %s", location); //$NON-NLS-1$
+		Activator.log(msg, error);
 	}
 
 	@Override
-	public void addMarker(ProblemMarkerInfo info) {
-		super.addMarker(info);
+	public void initPreferences(IProblemWorkingCopy problem) {
+		super.initPreferences(problem);
+		this.problemIds.add(problem.getId());
+		getLaunchModePreference(problem).enableInLaunchModes(CheckerLaunchMode.RUN_ON_DEMAND,
+				CheckerLaunchMode.RUN_ON_FILE_OPEN, CheckerLaunchMode.RUN_ON_FILE_SAVE);
+	}
+
+	private void addPreference(IProblemWorkingCopy problem, SingleConfigurationSetting<?> setting) {
+		IProblemPreference descriptor = (IProblemPreference) setting.getDescriptor();
+		addPreference(problem, descriptor, setting.getDefaultValue());
+	}
+
+	@Override
+	protected void setDefaultPreferenceValue(IProblemWorkingCopy problem, String key, Object defaultValue) {
+		MapProblemPreference map = getTopLevelPreference(problem);
+		map.setChildValue(key, defaultValue);
+	}
+
+	@Override
+	public RootProblemPreference getTopLevelPreference(IProblem problem) {
+		RootProblemPreference map = (RootProblemPreference) problem.getPreference();
+		if (map == null) {
+			map = this.preferences;
+			if (problem instanceof IProblemWorkingCopy) {
+				((IProblemWorkingCopy) problem).setPreference(map);
+			}
+		}
+		return map;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.cdt.core.IMarkerGenerator#addMarker(org.eclipse.cdt.core.
+	 * ProblemMarkerInfo)
+	 * 
+	 * NB: this method can only handle ProblemMarkerInfo from
+	 * fr.jayacode.rapider.checker.cxx.parser.ErrorParser
+	 */
+	public void addMarker(final ProblemMarkerInfo info) {
+		RapiderProblemMarkerInfo castedInfo = (RapiderProblemMarkerInfo) info;
+		String replacementText = REPLACEMENT_TEXT_PREFIX + castedInfo.getReplacementText();
+		reportProblem(mapProblemId(castedInfo), createProblemLocation(info), info.description, replacementText);
+	}
+
+	@Deprecated
+	@Override
+	public void addMarker(IResource file, int lineNumber, String description, int severity, String variableName) {
+		// We should not pass here
+	}
+
+	protected IProblemLocation createProblemLocation(ProblemMarkerInfo info) {
+		IProblemLocationFactory factory = CodanRuntime.getInstance().getProblemLocationFactory();
+		return factory.createProblemLocation((IFile) info.file, info.startChar, info.endChar, info.lineNumber);
 	}
 
 	/*
@@ -68,7 +241,6 @@ public class Checker extends AbstractExternalToolBasedChecker implements IChecke
 	 * org.eclipse.cdt.codan.core.cxx.externaltool.AbstractExternalToolBasedChecker#
 	 * getParserIDs()
 	 */
-	@Override
 	protected String[] getParserIDs() {
 		return new String[] { "fr.jayacode.rapider.checker.cxx.parser" }; //$NON-NLS-1$
 	}
@@ -82,21 +254,13 @@ public class Checker extends AbstractExternalToolBasedChecker implements IChecke
 	}
 
 	/*
-	 * (non-Javadoc)
+	 * (non-Javadoc) Filtre pour les commentaires de suppression du problème : si la
+	 * ligne contient le commentaire, alors on ne remonte pas le problème comme
+	 * étant remonté à l'utilisateur
 	 * 
-	 * @see
-	 * org.eclipse.cdt.codan.core.cxx.externaltool.AbstractExternalToolBasedChecker#
-	 * getReferenceProblemId()
-	 */
-	@Override
-	protected String getReferenceProblemId() {
-		return ERROR_PROBLEM_ID;
-	}
-
-	/* (non-Javadoc)
-	 * Filtre pour les commentaires de suppression du problème : si la ligne contient le commentaire, alors on ne remonte pas le problème
-	 * comme étant remonté à l'utilisateur
-	 * @see org.eclipse.cdt.codan.core.model.AbstractCheckerWithProblemPreferences#shouldProduceProblem(org.eclipse.cdt.codan.core.model.IProblem, org.eclipse.cdt.codan.core.model.IProblemLocation, java.lang.Object[])
+	 * @see org.eclipse.cdt.codan.core.model.AbstractCheckerWithProblemPreferences#
+	 * shouldProduceProblem(org.eclipse.cdt.codan.core.model.IProblem,
+	 * org.eclipse.cdt.codan.core.model.IProblemLocation, java.lang.Object[])
 	 */
 	@Override
 	protected boolean shouldProduceProblem(IProblem problem, IProblemLocation loc, Object... args) {
@@ -113,7 +277,6 @@ public class Checker extends AbstractExternalToolBasedChecker implements IChecke
 			InputStream content = file.getContents();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(content));
 			String line = new String();
-			int n = loc.getLineNumber();
 			for (int i = 0; i < loc.getLineNumber(); i++) {
 				line = reader.readLine();
 			}
@@ -124,4 +287,23 @@ public class Checker extends AbstractExternalToolBasedChecker implements IChecke
 		return super.shouldProduceProblem(problem, loc, args);
 	}
 
+	/**
+	 * Finds a registered problem id that corresponds to the marker id. A simple
+	 * string comparison is done. If no id is found, then a generic
+	 * info/warning/error id is returned
+	 * 
+	 * @param info
+	 * @return the id of the corresponding registered problem.
+	 */
+	private static String mapProblemId(final RapiderProblemMarkerInfo info) {
+
+		switch (info.severity) {
+		case IMarkerGenerator.SEVERITY_INFO:
+			return GENERIC_INFO_ID;
+		case IMarkerGenerator.SEVERITY_WARNING:
+			return GENERIC_WARNING_ID;
+		default:
+			return GENERIC_ERROR_ID;
+		}
+	}
 }
--- bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/parser/ErrorParser.java
+++ bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/parser/ErrorParser.java
@@ -9,25 +9,37 @@ import java.util.regex.Pattern;
 import org.eclipse.cdt.core.ErrorParserManager;
 import org.eclipse.cdt.core.IErrorParser;
 import org.eclipse.cdt.core.IMarkerGenerator;
-import org.eclipse.cdt.core.ProblemMarkerInfo;
+import fr.jayacode.rapider.checker.cxx.checker.RapiderProblemMarkerInfo;
 import org.eclipse.core.resources.IFile;
 
 /**
+ * Class in charge of processing outstream from rapider into problem markers.
+ * 
  * @author cconversin
- *
  */
 public class ErrorParser implements IErrorParser {
 
 	// sample line to parse:
 	//
-	// /path/to/project/src/HelloWorld.cpp|1|1|7|erreur type1
-	// -----------------1----------------- 2 3 4 -----5------
+	// /path/to/project/src/HelloWorld.cpp|charStart|charEnd|règle|sévérité|description de l'erreur|ReplacementText
+	// -----------------1----------------- ----2---- ---3--- --4-- ----5--- ----------6------------ -------7-------
 	//
-	private static Pattern lineParsingPattern = Pattern.compile("(.*)\\|(\\d+)\\|(\\d+)\\|(\\d+)\\|(.*)"); //$NON-NLS-1$
+	//
+	private static Pattern lineParsingPattern = Pattern.compile("(.*)\\|(\\d+)\\|(\\d+)\\|(.*)\\|(.*)\\|(.*)\\|(.*)"); //$NON-NLS-1$
 
-	/**
-	 * 
-	 */
+	// Indexes of value in pattern
+	private static final int FILE_PATH_GROUP_INDEX = 1;
+	private static final int OFFSET_GROUP_INDEX = 2;
+	private static final int LENGTH_GROUP_INDEX = 3;
+	private static final int RULE_NAME_GROUP_INDEX = 4;
+	private static final int SEVERITY_GROUP_INDEX = 5;
+	private static final int DESCRIPTION_GROUP_INDEX = 6;
+	private static final int REPLACEMENT_TEXT_GROUP_INDEX = 7;
+
+	// Severity keywords in Rapider's world
+	private static final String RAPIDER_INFO_VALUE = "info"; //$NON-NLS-1$
+	private static final String RAPIDER_WARNING_VALUE = "warning"; //$NON-NLS-1$
+
 	public ErrorParser() {
 	}
 
@@ -41,22 +53,39 @@ public class ErrorParser implements IErrorParser {
 	public boolean processLine(String line, ErrorParserManager eoParser) {
 		Matcher matcher = lineParsingPattern.matcher(line);
 		if (!matcher.matches()) {
+			// TODO log
 			return false;
 		}
-		String group1 = matcher.group(1);
-		String group2 = matcher.group(2);
-		String group3 = matcher.group(3);
-		String group5 = matcher.group(5);
-		String group4 = matcher.group(4);
+		String group1 = matcher.group(FILE_PATH_GROUP_INDEX);
 		IFile fileName = eoParser.findFileName(group1);
 		if (fileName != null) {
-			int lineNumber = Integer.parseInt(matcher.group(2));
-			String description = matcher.group(5);
-			ProblemMarkerInfo info = new ProblemMarkerInfo(fileName, lineNumber, description, IMarkerGenerator.SEVERITY_ERROR_RESOURCE, null);
+			int startChar = Integer.parseInt(matcher.group(OFFSET_GROUP_INDEX));
+			int endChar = Integer.parseInt(matcher.group(LENGTH_GROUP_INDEX));
+			String ruleName = matcher.group(RULE_NAME_GROUP_INDEX);
+			String description = matcher.group(DESCRIPTION_GROUP_INDEX);
+			int severity = mapSeverity(matcher.group(SEVERITY_GROUP_INDEX));
+			String replacementText = matcher.group(REPLACEMENT_TEXT_GROUP_INDEX);
+			// TODO log error and return false if infos are missing
+			RapiderProblemMarkerInfo info = new RapiderProblemMarkerInfo(fileName, startChar, endChar, severity,
+					ruleName, description, replacementText);
 			eoParser.addProblemMarker(info);
 			return true;
 		}
 		return false;
 	}
 
+	/** Maps rapider severity keywords with Eclipse internal severity values
+	 * @param severityFromRapider
+	 * @return
+	 */
+	private static int mapSeverity(final String severityFromRapider) {
+		switch (severityFromRapider) {
+		case RAPIDER_INFO_VALUE:
+			return IMarkerGenerator.SEVERITY_INFO;
+		case RAPIDER_WARNING_VALUE:
+			return IMarkerGenerator.SEVERITY_WARNING;
+		default:
+			return IMarkerGenerator.SEVERITY_ERROR_RESOURCE;
+		}
+	}
 }
