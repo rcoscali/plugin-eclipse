07f85e51523d1520031cf5a5eeee25c19be0dc15
cconversin <cconversin@gmail.com>
2018-02-10 09:09:09 +0100

    Génération d'un Id pour chaque diagnostic

--- bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/model/Diagnostic.java
+++ bundles/fr.jayacode.rapider.checker.cxx/src/fr/jayacode/rapider/checker/cxx/model/Diagnostic.java
@@ -4,6 +4,7 @@
 package fr.jayacode.rapider.checker.cxx.model;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author cconversin
@@ -11,8 +12,19 @@ import java.util.List;
  */
 public class Diagnostic {
 
+	private static AtomicInteger uidGenerator = new AtomicInteger(); 
+	private int uid;
 	public String DiagnosticName;
 	public List<Replacement> Replacements;
+
+	public Diagnostic() {
+		super();
+		this.uid = uidGenerator.getAndIncrement();
+	}
+
+	public int getId() {
+		return this.uid;
+	}
 
 	public String getDiagnosticName() {
 		return this.DiagnosticName;
